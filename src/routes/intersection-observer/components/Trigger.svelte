5bb24c99938d3e20886712585900f37251435eb4
gaetabno <88724808+gaetabno@users.noreply.github.com>
2024-07-28 19:32:59 +0200

    Merge branch 'master' of https://github.com/gaetabno/sandbox

--- src/routes/intersection-observer/components/Trigger.svelte
+++ src/routes/intersection-observer/components/Trigger.svelte
@@ -1,35 +1,4 @@
 <script>
-<<<<<<< HEAD
-  import { onMount } from "svelte";
-
-  export let sec;
-  let el;
-  let watch = false;
-  const color = Math.floor(Math.random() * 16777215).toString(16); 
-
-  onMount(() => {
-    const callback = (entries) =>  watch = entries[0].isIntersecting; 
-
-    const observer = new IntersectionObserver(callback, { threshold: 0.5 });
-
-    observer.observe(el);
-
-    return () => observer.unobserve(el);
-  });
-</script>
-
-<div class="min-h-screen grid place-content-center">
-  <div
-    style={`background:#${color}`}
-    class={`js-trigger c-trigger w-60 h-60 grid place-content-center ${sec}`}
-    class:watch
-    bind:this={el}
-  >
-    {sec}
-  </div> 
-  {console.log(watch)}
-</div>
-=======
   export let trigger;
   import Observer from "$lib/components/Observer.svelte";
   let color = Math.floor(Math.random() * 16777215).toString(16);
@@ -51,7 +20,6 @@
     </div>
   </div>
 </Observer>
->>>>>>> 4352c5c0b68effc08304ff7eab59a1908d965e8a
 
 <style>
   .c-trigger {
